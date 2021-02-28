f0821992579d8e6865cfbcc8a824950f47e165f4
fcs <fredericcazesulfourt@yahoo.fr>
2021-02-28 17:10:50 +0100

    Optimisation du code

--- GigHub/Controllers/AccountController.cs
+++ GigHub/Controllers/AccountController.cs
@@ -81,7 +81,7 @@ namespace GigHub.Controllers
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, model.RememberMe });
                 case SignInStatus.Failure:
                 default:
                     ModelState.AddModelError("", "Invalid login attempt.");
@@ -318,7 +318,7 @@ namespace GigHub.Controllers
             {
                 return View("Error");
             }
-            return RedirectToAction("VerifyCode", new { Provider = model.SelectedProvider, ReturnUrl = model.ReturnUrl, RememberMe = model.RememberMe });
+            return RedirectToAction("VerifyCode", (Provider: model.SelectedProvider, model.ReturnUrl, model.RememberMe));
         }
 
         //
